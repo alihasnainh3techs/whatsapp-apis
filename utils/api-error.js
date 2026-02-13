bccd8d582c0213e142ff9dd8f1a60ea09dcd13e2
alihasnainh3techs <alimurtaza32721@gmail.com>
2026-02-13 11:28:05 +0500

    change in utils

--- utils/api-error.js
+++ utils/api-error.js
@@ -1,5 +1,6 @@
 export class APIError {
   constructor(statusCode = 500, message = 'Internal server error') {
+    this.success = false;
     this.statusCode = statusCode;
     this.message = message;
   }
--- utils/api-response.js
+++ utils/api-response.js
@@ -1,5 +1,6 @@
 export class APIResponse {
   constructor(statusCode = 200, message = '', data = {}) {
+    this.success = true;
     this.statusCode = statusCode;
     this.message = message;
     this.data = data;
